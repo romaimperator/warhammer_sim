606666cca9884089b0e9703c023f474c15bfc093
Daniel Fox <romaimperator@gmail.com>
2014-08-29 17:06:49 -0500

    Reordering methods and adding tests for the Unit class

---
+++ spec/unit_spec.rb
@@ -0,0 +1,305 @@
+require File.expand_path(File.dirname(__FILE__) + "/../unit")
+
+class UnitFactory
+  attr_accessor :model,
+                :special_models,
+                :size,
+                :width,
+                :offset,
+                :equipment
+
+  def initialize(model_double)
+    @model = model_double
+    @special_models = []
+    @size = 10
+    @width = 5
+    @offset = 0
+    @equipment = []
+  end
+
+  def build
+    Unit.new(@model, @special_models, @size, @width, @offset, @equipment)
+  end
+
+  def model(model)
+    @model = model
+    self
+  end
+
+  def special_models(special_models)
+    @special_models = special_models
+    self
+  end
+
+  def size(size)
+    @size = size
+    self
+  end
+
+  def width(width)
+    @width = width
+    self
+  end
+
+  def offset(offset)
+    @offset = offset
+    self
+  end
+
+  def equipment(equipment)
+    @equipment = equipment
+    self
+  end
+end
+
+describe Unit do
+  subject { UnitFactory.new(double(:model,
+                                   :mm_width => 20,
+                                   :mm_length => 20,
+                                   :name => "halberdier")) }
+
+  describe "#check_break_test" do
+    let(:model) { double(:model, :leadership => 7) }
+    let(:unit) { subject.model(model).size(20).build }
+
+    it "returns true if the test is passed" do
+      roll = 6
+      modifier = -1
+      expect(unit.check_break_test(roll, modifier)).to be_true
+    end
+
+    it "returns true if the roll is snake eyes" do
+      roll = 2
+      modifier = -100
+      expect(unit.check_break_test(roll, modifier)).to be_true
+    end
+
+    it "returns false if the unit fails the test" do
+      roll = 8
+      modifier = -1
+      expect(unit.check_break_test(roll, modifier)).to be_false
+    end
+  end
+
+  describe "#convert_coordinate" do
+    let(:unit) { subject.offset(40).width(10).build }
+
+    it "converts the coordinate of the opposing unit to the given unit" do
+      expect(unit.convert_coordinate(100)).to eq(140)
+      expect(unit.convert_coordinate(20)).to eq(220)
+      expect(unit.convert_coordinate(200)).to eq(40)
+    end
+  end
+
+  describe "#dead?" do
+    it "is true when there are no models left" do
+      unit = subject.size(0).build
+      expect(unit.dead?).to be_true
+    end
+
+    it "is false when there models left" do
+      unit = subject.build
+      expect(unit.dead?).to be_false
+    end
+  end
+
+  describe "#farthest_back_special_model_occupied_space" do
+    let(:special_models) do
+      { [1, 5] => double(:special_models, :mm_width => 40, :mm_length => 40) }
+    end
+
+    it "returns the rank of the farthest back special model space" do
+      unit = subject.size(20).special_models(special_models).build
+      expect(unit.farthest_back_special_model_occupied_space).to eq(2)
+    end
+
+    it "returns the rank even when there aren't regular models in the rank" do
+      unit = subject.size(1).special_models(special_models).build
+      expect(unit.farthest_back_special_model_occupied_space).to eq(2)
+    end
+  end
+
+  describe "#is_horde?" do
+    it "returns true if the width is wide enough" do
+      unit = subject.width(10).build
+      expect(unit.is_horde?).to be_true
+    end
+
+    it "returns true if special models fill in the missing regular models" do
+      special_models = { [1, 5] => double(:special_models, :mm_width => 40) }
+      unit = subject.width(10).size(8).special_models(special_models).build
+      expect(unit.is_horde?).to be_true
+    end
+
+    it "returns false if not wide enough" do
+      unit = subject.width(5).build
+      expect(unit.is_horde?).to be_false
+    end
+
+    it "returns false even if wide enough if there aren't enough models" do
+      unit = subject.width(10).size(9).build
+      expect(unit.is_horde?).to be_false
+    end
+  end
+
+  describe "#is_steadfast?" do
+    let(:unit) { subject.size(20).build }
+
+    it "returns true when the unit has more ranks than the defender" do
+      defender_ranks = 3
+      expect(unit.is_steadfast?(defender_ranks)).to be_true
+    end
+
+    it "returns false when the unit doesn't" do
+      defender_ranks = 4
+      expect(unit.is_steadfast?(defender_ranks)).to be_false
+    end
+  end
+
+  describe "#left_flank_location" do
+    it "is defined as zero" do
+      unit = subject.build
+      expect(unit.left_flank_location).to eq(0)
+    end
+  end
+
+  describe "#mm_width" do
+    it "returns the width of the unit in millimeters" do
+      unit = subject.width(5).build
+      expect(unit.mm_width).to eq(100)
+    end
+  end
+
+  describe "#model_count" do
+    it "returns the number of models in the unit including special models" do
+      special_models = { [1, 5] => double(:special_models, :mm_width => 40) }
+      unit = subject.size(20).special_models(special_models).build
+      expect(unit.model_count).to eq(21)
+    end
+  end
+
+  describe "#models_in_mm_range" do
+    let(:special_models) do
+      { [1, 5] => double(:special_model,
+                         :mm_width => 40,
+                         :mm_length => 40,
+                         :name => "champion") }
+    end
+    let(:unit) do
+      subject.special_models(special_models).build
+    end
+
+    it "finds the models that fall within the given range" do
+      expect(unit.models_in_mm_range(0, 79)).to eq([unit.model])
+    end
+
+    it "finds both models and special models" do
+      expect(unit.models_in_mm_range(0, 100)).to eq([special_models[[1, 5]],
+                                                     unit.model])
+    end
+
+    it "finds the model on right if the second value is between two models" do
+      expect(unit.models_in_mm_range(80, 80)).to eq([special_models[[1, 5]],
+                                                     unit.model])
+    end
+
+    it "finds the model on left if the first value is between two models" do
+      expect(unit.models_in_mm_range(80, 80)).to eq([special_models[[1, 5]],
+                                                     unit.model])
+    end
+
+    it "doesn't return duplicate models" do
+      expect(unit.models_in_mm_range(0, 200)).to eq([special_models[[1, 5]],
+                                                     unit.model])
+    end
+  end
+
+  describe "#number_of_ranks" do
+    it "returns the number of ranks whether full or not" do
+      special_models = { [1, 5] => double(:special_models,
+                                          :mm_width => 40,
+                                          :mm_length => 100) }
+      unit = subject.size(10).special_models(special_models).build
+      expect(unit.number_of_ranks).to eq(5)
+    end
+
+    it "returns the number of ranks when there are many many models" do
+      unit = subject.width(10).size(100).build
+      expect(unit.number_of_ranks).to eq(10)
+    end
+  end
+
+  describe "#positions_occupied" do
+    it "returns the number of unit positions that are occupied" do
+      special_models = { [1, 5] => double(:special_models,
+                                          :mm_width => 40,
+                                          :mm_length => 40) }
+      unit = subject.size(20).special_models(special_models).build
+      expect(unit.positions_occupied).to eq(24)
+    end
+  end
+
+  describe "#rank_bonus" do
+    it "returns the rank bonus of the unit" do
+      unit = subject.size(20).width(5).build
+      expect(unit.rank_bonus).to eq(3)
+    end
+
+    it "returns the rank bonus when there are special models" do
+      special_models = { [1, 2] => double(:special_models,
+                                          :mm_width => 40,
+                                          :mm_length => 40) }
+      unit = subject.size(11).width(5).special_models(special_models).build
+      expect(unit.rank_bonus).to eq(2)
+    end
+  end
+
+  describe "#right_flank_location" do
+    it "is the left flank plus the width of unit in millimeters" do
+      unit = subject.width(10).build
+      expect(unit.right_flank_location).to eq(200)
+    end
+  end
+
+  describe "#special_model_occupied_spaces_in_rank" do
+    let(:special_models) do
+      { [1, 2] => double(:special_models, :mm_width => 40, :mm_length => 40) }
+    end
+    let(:unit) { subject.size(40).special_models(special_models).build }
+
+    it "returns 2 spaces for the first rank" do
+      expect(unit.special_model_occupied_spaces_in_rank(1)).to eq(2)
+    end
+
+    it "returns 2 spaces for the second rank" do
+      expect(unit.special_model_occupied_spaces_in_rank(2)).to eq(2)
+    end
+
+    it "returns 0 spaces for the third rank" do
+      expect(unit.special_model_occupied_spaces_in_rank(3)).to eq(0)
+    end
+
+    it "returns 0 spaces for the fourth rank" do
+      expect(unit.special_model_occupied_spaces_in_rank(4)).to eq(0)
+    end
+  end
+
+ #describe "#model_in_position" do
+ #  let(:model) { double(:model, :mm_width => 20, :mm_length => 20) }
+ #  let(:special_models) do
+ #    { [1, 5] => double(:character, :mm_width => 20, :mm_length => 20) }
+ #  end
+ #  let(:unit) do
+ #    UnitFactory.new.model(model).special_models(special_models).build
+ #  end
+
+ #  it "returns a special model when the model is in the position" do
+ #    expect(unit.model_in_position(1, 5)).to eq(special_models[[1, 5]])
+ #  end
+
+ #  it "returns the unit model otherwise" do
+ #    expect(unit.model_in_position(2, 5)).to eq(model)
+ #  end
+ #end
+end
+
--- unit.rb
+++ unit.rb
@@ -3,6 +3,86 @@ require_relative 'compute_wound_needed'
 
 class Unit < Struct.new(:model, :special_models, :size, :width, :offset, :equipment)
   FIRST_RANK = 1
+  INSANE_COURAGE_ROLL = 2
+
+  attr_accessor :hits
+  attr_accessor :unsaved_wounds
+  attr_accessor :wounds_caused
+
+  def initialize(*args, &block)
+    super
+    @hits = 0
+    @unsaved_wounds = 0
+    @wounds_caused = 0
+  end
+
+  def banner
+    0
+  end
+
+  def charge
+    0
+  end
+
+  def check_break_test(roll, modifier)
+    check_leadership_test(roll, modifier) || roll == INSANE_COURAGE_ROLL
+  end
+
+  def check_leadership_test(roll, modifier)
+    roll <= model.leadership + modifier
+  end
+
+  def combat_res_earned
+    rank_bonus + banner + wounds_caused + overkill + charge + flank_or_rear
+  end
+
+  def convert_coordinate(value)
+    mm_width - value + offset
+  end
+
+  def dead?
+    size <= 0
+  end
+
+  def farthest_back_special_model_occupied_space
+    special_models.map do |position, special_model|
+      position[0] + special_model.mm_length / model.mm_length - 1
+    end.max || 0
+  end
+
+  def find_targets(target_unit)
+    rank_targets =
+      (1..number_of_ranks).map do |rank|
+        (1..width).map do |file|
+          model_to_check = model_in_position(rank, file)
+          if !model_to_check.nil?
+            target_unit.models_in_mm_range(
+              target_unit.convert_coordinate(model.mm_width * (file - 1)),
+              target_unit.convert_coordinate(model.mm_width * file)
+            )
+          else
+            []
+          end
+        end
+      end
+    rank_targets.map { |rank_positions|
+      count_by(rank_positions) { |position| position }
+    }
+    #Hash[rank_targets.group_by { |position_targets|
+    #  position_targets[0]
+    #}.map { |rank, position_targets|
+    #  [
+    #    rank,
+    #    count_by(position_targets.map { |position_target|
+    #      position_target[1]
+    #    }) { |element| element }
+    #  ]
+    #}]
+  end
+
+  def flank_or_rear
+    0
+  end
 
   def for_each_item(&block)
     equipment.each do |item|
@@ -10,63 +90,39 @@ class Unit < Struct.new(:model, :special_models, :size, :width, :offset, :equipm
     end
   end
 
-  def dead?
-    size <= 0
+  def is_horde?
+    width >= 10 &&
+      size + special_model_occupied_spaces_in_rank(FIRST_RANK) >= 10
   end
 
-  def model_in_position(rank, file)
-    found_model = nil
-    special_models.each do |position, special_model|
-      if model_covers_position?(special_model, position, rank, file)
-        found_model = special_model
-      end
+  def is_steadfast?(defender_ranks)
+    number_of_ranks > defender_ranks
+  end
+
+  def left_flank_location
+    0
+  end
+
+  def method_missing(name, *args)
+    model.send(name, *args)
+  end
+
+  def mm_width
+    width * model.mm_width
+  end
+
+  def model_count
+    size + special_models.size
+  end
+
+  def models_in_base_contact(defender)
+    if mm_width > defender.mm_width
+      defender.mm_width / model.mm_width + 2
+    elsif mm_width < defender.mm_width
+      width
+    else
+      width
     end
-    found_model || model
-  end
-
-  def find_targets(target_unit)
-    rank_targets =
-      (1..size).map do |model_number|
-        rank = ((model_number - 1) / width) + 1
-        file = ((model_number - 1) % width) + 1
-        model_to_check = model_in_position(rank, file)
-        [
-          rank,
-          target_unit.models_in_mm_range(
-            target_unit.convert_coordinate(model.mm_width * (file - 1)),
-            target_unit.convert_coordinate(model.mm_width * file)
-          ),
-        ]
-      end
-    Hash[rank_targets.group_by { |position_targets|
-      position_targets[0]
-    }.map { |rank, position_targets|
-      [
-        rank,
-        count_by(position_targets.map { |position_target|
-          position_target[1]
-        }) { |element| element }
-      ]
-    }]
-  end
-
-  def count_by(array, &block)
-    Hash[array.group_by { |element|
-      block.call(element)
-    }.map { |key, value|
-      [key, value.size]
-    }]
-  end
-
-  def model_covers_position?(special_model, position, rank, file)
-    position[1] <= file &&
-      file < position[1] + special_model.mm_width / model.mm_width &&
-      position[0] <= rank &&
-      rank < position[0] + special_model.mm_length / model.mm_length
-  end
-
-  def convert_coordinate(value)
-    mm_width - value + offset
   end
 
   def models_in_mm_range(first, second)
@@ -80,55 +136,16 @@ class Unit < Struct.new(:model, :special_models, :size, :width, :offset, :equipm
     #Hash[models.group_by { |model| model }.map { |key, value| [key, value.size] }]
   end
 
-  def find_position_from_mm(value, left=false)
-    if value % model.mm_width == 0
-      position = value / model.mm_width
-      [left ? position : position + 1, 1].max
-    else
-      (value.to_f / model.mm_width).ceil
-    end
+  def number_of_ranks
+    final_possible_rank = [positions_occupied / width +
+                           (positions_occupied % width >= 1 ? 1 : 0),
+                           farthest_back_special_model_occupied_space].max
+
+    final_possible_rank
   end
 
-  def right_flank_location
-    left_flank_location + mm_width
-  end
-
-  def left_flank_location
+  def overkill
     0
-  end
-
-  def models_in_base_contact(defender)
-    if mm_width > defender.mm_width
-      defender.mm_width / model.mm_width + 2
-    elsif mm_width < defender.mm_width
-      width
-    else
-      width
-    end
-  end
-
-  def mm_width
-    width * model.mm_width
-  end
-
-  def is_horde?
-    width >= 10
-  end
-
-  def take_wounds(unsaved_wounds)
-    self.size = [self.size - unsaved_wounds, 0].max
-  end
-
-  def method_missing(name, *args)
-    model.send(name, *args)
-  end
-
-  def combat_res_earned
-    rank_bonus + banner + wounds_caused + overkill + charge + flank_or_rear
-  end
-
-  def model_count
-    size + special_models.size
   end
 
   def positions_occupied
@@ -139,9 +156,43 @@ class Unit < Struct.new(:model, :special_models, :size, :width, :offset, :equipm
     end
   end
 
+  def rank_bonus
+    remaining_regular_models = size
+    rank = 1
+    (1..number_of_ranks).each do |current_rank|
+      occupied_spaces = special_model_occupied_spaces_in_rank(current_rank)
+      rank = current_rank if remaining_regular_models + occupied_spaces >= 5
+      remaining_regular_models -= (width - occupied_spaces)
+    end
+    [rank - 1, 3].min
+  end
+
+  def right_flank_location
+    left_flank_location + mm_width
+  end
+
+  def roll_break_test(modifier, defender_ranks)
+    result = sum_roll(2)
+    if is_steadfast?(defender_ranks)
+      check_break_test(result, 0)
+    else
+      check_break_test(result, modifier)
+    end
+  end
+
+  def roll_flee
+    sum_roll(2)
+  end
+
+  def roll_pursuit
+    sum_roll(2)
+  end
+
   def special_model_occupied_spaces_in_rank(rank)
     special_models.inject(0) do |sum, special_model|
       if special_model[0][0] == rank
+        sum + special_model[1].mm_width / model.mm_width
+      elsif special_model[0][0] + special_model[1].mm_length / model.mm_length - 1 == rank
         sum + special_model[1].mm_width / model.mm_width
       else
         sum
@@ -149,90 +200,44 @@ class Unit < Struct.new(:model, :special_models, :size, :width, :offset, :equipm
     end
   end
 
-  def farthest_back_special_model_occupied_space
-    special_models.map do |position, special_model|
-      position[0] + special_model.mm_length / model.mm_length - 1
-    end.max || 0
+  def take_wounds(unsaved_wounds)
+    self.size = [self.size - unsaved_wounds, 0].max
   end
 
-  def number_of_ranks
-    final_possible_rank = [positions_occupied / width +
-                           (positions_occupied % width >= 1 ? 1 : 0),
-                           farthest_back_special_model_occupied_space].max
+private
 
-    final_possible_rank
+  def count_by(array, &block)
+    Hash[array.group_by { |element|
+      block.call(element)
+    }.map { |key, value|
+      [key, value.size]
+    }]
   end
 
-  def rank_bonus
-    remaining_regular_models = size
-    rank = 1
-    (1..number_of_ranks).each do |current_rank|
-      occupied_spaces = special_model_occupied_spaces_in_rank(rank)
-      rank = current_rank if remaining_regular_models + occupied_spaces >= 5
-      remaining_regular_models -= (width - occupied_spaces)
-    end
-    [rank - 1, 3].min
-  end
-
-  def banner
-    0
-  end
-
-  def wounds_caused
-    @wounds_caused ||= 0
-  end
-
-  def wounds_caused=(new_value)
-    @wounds_caused = new_value
-  end
-
-  def hits
-    @hits||= 0
-  end
-
-  def hits=(new_value)
-    @hits= new_value
-  end
-
-  def unsaved_wounds
-    @unsaved_wounds ||= 0
-  end
-
-  def unsaved_wounds=(new_value)
-    @unsaved_wounds = new_value
-  end
-
-  def overkill
-    0
-  end
-
-  def charge
-    0
-  end
-
-  def flank_or_rear
-    0
-  end
-
-  def roll_break_test(modifier, defender_ranks)
-    result = sum_roll(2)
-    if is_steadfast?(defender_ranks)
-      result > model.leadership && result != 2
+  def find_position_from_mm(value, left=false)
+    if value % model.mm_width == 0
+      position = value / model.mm_width
+      [left ? position : position + 1, 1].max
     else
-      result - modifier > model.leadership && result != 2
+      (value.to_f / model.mm_width).ceil
     end
   end
 
-  def is_steadfast?(defender_ranks)
-    number_of_ranks > defender_ranks
+  def model_covers_position?(special_model, position, rank, file)
+    position[1] <= file &&
+      file < position[1] + special_model.mm_width / model.mm_width &&
+      position[0] <= rank &&
+      rank < position[0] + special_model.mm_length / model.mm_length
   end
 
-  def roll_pursuit
-    sum_roll(2)
-  end
-
-  def roll_flee
-    sum_roll(2)
+  def model_in_position(rank, file)
+    found_model = nil
+    special_models.each do |position, special_model|
+      if model_covers_position?(special_model, position, rank, file)
+        found_model = special_model
+      end
+    end
+    found_model || model
   end
 end
 
